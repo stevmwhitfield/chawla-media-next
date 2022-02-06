1392d4681d44303c2bb1327a1e4b2d479bf9a567
BuildTools <unconfigured@null.spigotmc.org>
2022-02-06 10:45:45 -0500

    Basic set up

---
+++ components/Head.js
@@ -0,0 +1,17 @@
+import Head from "next/head";
+
+const CustomHead = () => {
+  return (
+    <Head>
+      <link
+        rel="stylesheet"
+        href="https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.min.css"
+        integrity="sha512-NhSC1YmyruXifcj/KFRWoC561YpHpc5Jtzgvbuzx5VozKpWvQ+4nXhPdFgmx8xqexRcpAglTj9sIBWINXa8x5w=="
+        crossOrigin="anonymous"
+        referrerPolicy="no-referrer"
+      />
+    </Head>
+  );
+};
+
+export default CustomHead;
