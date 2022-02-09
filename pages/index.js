b085768c1eb8c6dd760df2deda68081228083295
BuildTools <unconfigured@null.spigotmc.org>
2022-02-09 10:43:30 -0500

    Start third section of home page

--- pages/index.js
+++ pages/index.js
@@ -1,22 +1,17 @@
 import Layout from "../components/Layout/Layout";
 import Section1 from "../components/Home/Section1";
 import Section2 from "../components/Home/Section2";
+import Section3 from "../components/Home/Section3";
 
 const HomePage = () => {
   return (
     <Layout>
       <Section1 />
       <Section2 />
+      <Section3 />
+      {/* <Section4 /> */}
     </Layout>
   );
 };
 
 export default HomePage;
-
-// Page layout
-// <Header />
-// <Section1 /> -> <Gallery direction="horizontal" />
-// <Section2 /> -> <Button type="default"/>
-// <Section3 />
-// <Section4 /> -> <Testimonial />
-// <Footer />
