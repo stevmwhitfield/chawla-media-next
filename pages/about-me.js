6aac5033d0169874c35bdb12e3f038f6a3c6ee06
Steven Whitfield <stevmwhitfield@gmail.com>
2022-02-19 10:21:34 -0500

    Complete responsive about page

--- pages/about-me.js
+++ pages/about-me.js
@@ -1,5 +1,7 @@
+import Image from "next/image";
 import CustomHead from "../components/Head/Head";
 import Layout from "../components/Layout/Layout";
+import styles from "../styles/About/About.module.scss";
 
 const AboutPage = () => {
   return (
@@ -10,7 +12,36 @@ const AboutPage = () => {
         url="https://www.chawlamedia.com/about-me"
       />
       <Layout>
-        <h1>About page coming soon!</h1>
+        <section className="pageHeader">
+          <h1>About Me</h1>
+          <h2>Sunishth Chawla</h2>
+        </section>
+        <section className={styles.container}>
+          <article id={styles.imageContainer}>
+            <Image src="/sunishth-chawla.jpg" width={500} height={500} />
+          </article>
+          <article id={styles.contentContainer}>
+            <p>
+              It all started when my dad would have me use his huge DSLRs to
+              take pictures of the family. Every time I would take a picture, it
+              would be blurry, uncentered, and nowhere near professional. Over
+              the course of five years, I have developed the technique of
+              photography; whether it be of cars, human subjects, or nature.
+              Having just bought my first, "very own" camera, I have finally
+              gotten the freedom to travel to many parts of the country that I
+              never knew existed. Since then, I have been clicking away and
+              satisfying not only my clientele, but my own self as well.
+            </p>
+            <p>
+              Alongside being your photographer, I want to teach you what it is
+              like to pick up a hobby like mine and actually be able to go forth
+              with it. The process of picking up a camera and taking a picture
+              is intricate enough, but there is a lot behind behind the scenes
+              that goes on to capture that vivid shot that we want to show our
+              friends, family, and the whole world.
+            </p>
+          </article>
+        </section>
       </Layout>
     </>
   );
