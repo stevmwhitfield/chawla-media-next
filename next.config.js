d33de21e5096d212be07f61d8aaf8f83304f43fc
BuildTools <unconfigured@null.spigotmc.org>
2022-02-22 09:46:44 -0500

    Setup Sanity for prod

--- next.config.js
+++ next.config.js
@@ -1,6 +1,15 @@
+const STUDIO_REWRITE = {
+  source: "/chawla-media-cms/:path*",
+  destination:
+    process.env.NODE_ENV === "development"
+      ? "http://localhost:3333/studio/:path*"
+      : "/sanity/index.html",
+};
+
 module.exports = {
   reactStrictMode: true,
   images: {
     domains: ["cdn.sanity.io"],
   },
+  rewrites: () => [STUDIO_REWRITE],
 };
