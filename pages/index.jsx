39fe07bbe393f0f30e17c20e66756d5f0a0888e1
Steven Whitfield <stevmwhitfield@gmail.com>
2022-08-12 18:48:46 -0400

    Add page header

--- pages/index.jsx
+++ pages/index.jsx
@@ -5,29 +5,32 @@ import ProductList from "../components/Shop/ProductList";
 import CategoryList from "../components/Shop/CategoryList";
 
 const ShopHomePage = ({ merchant, products, categories }) => {
-  return (
-    <>
-      <CustomHead
-        title="Shop | Chawla Media"
-        description="Purchase exclusive prints of your favorite photos."
-        url="https://shop.chawlamedia.com/"
-      />
-      <Layout>
-        <h1>{merchant.data[0].name}</h1>
-        <h2>Categories</h2>
-        <CategoryList categories={categories} />
-        <h2>Products</h2>
-        <ProductList products={products} />
-      </Layout>
-    </>
-  );
+	return (
+		<>
+			<CustomHead
+				title="Shop | Chawla Media"
+				description="Purchase exclusive prints of your favorite photos."
+				url="https://shop.chawlamedia.com/"
+			/>
+			<Layout>
+				{/* <h1>{merchant.data[0].name}</h1> */}
+				{/* <h2>Categories</h2> */}
+				{/* <CategoryList categories={categories} /> */}
+				{/* <h2>Products</h2> */}
+				<section className="pageHeader">
+					<h1>Shop</h1>
+				</section>
+				<ProductList products={products} />
+			</Layout>
+		</>
+	);
 };
 
 export default ShopHomePage;
 
 export async function getStaticProps() {
-  const merchant = await commerce.merchants.about();
-  const { data: products } = await commerce.products.list();
-  const { data: categories } = await commerce.categories.list();
-  return { props: { merchant, products, categories } };
+	const merchant = await commerce.merchants.about();
+	const { data: products } = await commerce.products.list();
+	const { data: categories } = await commerce.categories.list();
+	return { props: { merchant, products, categories } };
 }
