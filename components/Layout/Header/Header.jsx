31689929b19b99c421e0345a3d954018c691c3b9
Steven Whitfield <stevmwhitfield@gmail.com>
2022-08-16 09:37:54 -0400

    Fixed header

--- components/Layout/Header/Header.jsx
+++ components/Layout/Header/Header.jsx
@@ -1,22 +1,23 @@
 import { useWindowSize } from "../../../hooks/useWindowSize";
 import MobileNavigation from "./MobileNavigation";
 import DesktopNavigation from "./DesktopNavigation";
+import styles from "../../../styles/Layout/Header/Header.module.scss";
 
 const Header = () => {
-  const size = useWindowSize();
-  if (size.width < 1024 || size.height < 480) {
-    return (
-      <header>
-        <MobileNavigation />
-      </header>
-    );
-  } else {
-    return (
-      <header>
-        <DesktopNavigation />
-      </header>
-    );
-  }
+	const size = useWindowSize();
+	if (size.width < 1024 || size.height < 480) {
+		return (
+			<header>
+				<MobileNavigation />
+			</header>
+		);
+	} else {
+		return (
+			<header id={styles.header}>
+				<DesktopNavigation />
+			</header>
+		);
+	}
 };
 
 export default Header;
